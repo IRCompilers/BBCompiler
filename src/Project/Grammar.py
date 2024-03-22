7e8ff9c4b6b981c3198a0e61a26a6e179be75aba
Scorpion-Ceeb <sebastiansuarezgomez01@gmail.com>
2024-03-21 22:25:03 -0500

    Non Terminals grammar

--- src/Project/Grammar.py
+++ src/Project/Grammar.py
@@ -1,14 +1,37 @@
 from src.Common.Compiler import Grammar
 
 G = Grammar()
+
+
 # No Terminals
 program = G.NonTerminal("<program>", startSymbol=True)
 expression = G.NonTerminals("<expression>")
 statement = G.NonTerminal("<statement>")
-parameters = G.NonTerminal("<parameters>")
+parameters, parameter_list, variable = G.NonTerminals("<parameters> <parameter_list> <variable>")
 function_style = G.NonTerminal("<function_style>")
 type_def = G.NonTerminal("<type_def>")
-protocol_declare = G.NonTerminal("<protocol_declare>")
+protocol_declare, protocol_body = G.NonTerminals("<protocol_declare> <protocol_body>")
+simple_expression = G.NonTerminal("<simple_expression>")
+expression_block = G.NonTerminal("<expression_block>")
+class_block, class_body, class_declaration = G.NonTerminals("<class_block> <class_body> <class_declaration>")
+arguments, argument_list = G.NonTerminals("<arguments> <argument_list>")
+typed_parameters, typed_parameter_list = G.NonTerminals("<typed_parameters> <typed_parameter_list>")
+declaration = G.NonTerminal("<declaration>")
+if_block = G.NonTerminal("<if_block>")
+else_block = G.NonTerminal("<else_block>")
+disjunction, conjunction = G.NonTerminals("<disjunction> <conjunction>")
+literal, proposition, boolean = G.NonTerminals("<literal> <proposition> <boolean>")
+concatenation = G.NonTerminal("<concatenation>")
+arithmetic_expression = G.NonTerminal("<arithmetic_expression>")
+module, product, monomial, pow_exp = G.NonTerminals("<module> <product> <monomial> <pow_exp>")
+high_hierarchy_object, object_exp = G.NonTerminals("<high_hierarchy_object> <object_exp>")
+list_exp = G.NonTerminal("<list_exp>")
+
+
+
+
+
+
 
 # Terminals
 string, identifier, number = G.Terminals("<string> <id> <number>")
