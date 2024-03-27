4cb9a20033fba8fe81263a88f660bb880076d790
hmrguez <zealot.algo@gmail.com>
2024-03-27 11:57:08 -0400

    Finished testing polymorphism, protocols and inheritance

--- test/SemanticCheckTesting.py
+++ test/SemanticCheckTesting.py
@@ -230,7 +230,252 @@ class TestSemanticCheckerVisitor(unittest.TestCase):
                                   "The printp function doesn't exist in the current context",
                                   "The j variable doesn't exist in the current context"])
 
+    def test_simple_inheritance_polymorphism_correct(self):
+        scope = Scope()
+        base_type = TypeNode("Human", [], [])
+        sub_type = TypeNode("Person", [], [])
 
+        instantiation = NewNode("Person", [])
+        let_ = LetNode([ParameterNode("john", "Human")], [instantiation], VariableNode("john"))
+        node = ProgramNode([base_type, sub_type], let_)
+        result = self.visitor.visit(node, scope)
+        self.assertEqual(result, [])
+
+    def test_circular_inheritance(self):
+        scope = Scope()
+        type_a = TypeNode("TypeA", [], [], "TypeB")
+        type_b = TypeNode("TypeB", [], [], "TypeA")
+
+        node = ProgramNode([type_a, type_b], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+        self.assertEqual(result, ["Circular inheritance detected between TypeA and TypeB"])
+
+    def test_non_existing_parent(self):
+        scope = Scope()
+        type_a = TypeNode("TypeA", [], [], "TypeB")
+
+        node = ProgramNode([type_a], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+        self.assertEqual(result, ["The TypeB type doesn't exist in the current context"])
+
+    def test_inheritance_with_wrong_parent_params(self):
+        scope = Scope()
+        type_a = TypeNode("TypeA", [], [ParameterNode("name", "String")])
+        type_b = TypeNode("TypeB", [], [ParameterNode("name", "String")], "TypeA",
+                          [VariableNode("name"), NumberNode(1)])
+
+        node = ProgramNode([type_a, type_b], NumberNode(2))
+
+        result = self.visitor.visit(node, scope)
+        self.assertEqual(result, ["Missmatch of parent constructor parameters on inheritance of TypeB from TypeA"])
+
+    def test_base_call_no_inheritance(self):
+        scope = Scope()
+        function_body = FunctionCallNode("print", [FunctionCallNode("base", [])])
+        function_ = FunctionNode("greet", [ParameterNode("name", "String")], function_body)
+        type_a = TypeNode("person", [function_], [ParameterNode("name", "String")])
+
+        node = ProgramNode([type_a], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ["Parent doesn't have a definition for greet"])
+
+    def test_base_call_no_base_on_parent(self):
+        scope = Scope()
+        function_body = FunctionCallNode("print", [FunctionCallNode("base", [])])
+        function_ = FunctionNode("greet", [ParameterNode("name", "String")], function_body)
+        type_b = TypeNode("human", [], [])
+        type_a = TypeNode("person", [function_], [ParameterNode("name", "String")], "human", [])
+
+        node = ProgramNode([type_a, type_b], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ["Parent doesn't have a definition for greet"])
+
+    def test_base_call_correct(self):
+        scope = Scope()
+        simple_greet = FunctionNode("greet", [ParameterNode("name", "String")], VariableNode("name"))
+        greet_body = FunctionCallNode("print", [FunctionCallNode("base", [])])
+        greet = FunctionNode("greet", [ParameterNode("name", "String")], greet_body)
+
+        type_b = TypeNode("human", [simple_greet], [])
+        type_a = TypeNode("person", [greet], [ParameterNode("name", "String")], "human", [])
+
+        node = ProgramNode([type_a, type_b], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [])
+
+    def test_inheritance_from_reserved_types(self):
+        scope = Scope()
+
+        type_a = TypeNode("A", [], [], "void")
+        type_b = TypeNode("B", [], [], "Number")
+        type_c = TypeNode("C", [], [], "String")
+        type_d = TypeNode("D", [], [], "Boolean")
+
+        node = ProgramNode([type_a, type_b, type_d, type_c], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [
+            "Type A can't inherit from reserved type void",
+            "Type B can't inherit from reserved type Number",
+            "Type C can't inherit from reserved type String",
+            "Type D can't inherit from reserved type Boolean",
+        ])
+
+    def test_inheritance_accessing_parent_members(self):
+        scope = Scope()
+
+        human = TypeNode("human", [TypeAtributeNode(ParameterNode("a", "Number"), NumberNode(2))], [])
+
+        get_a_func = FunctionNode("get_a", [], SelfVariableNode(True, "a"))
+        person = TypeNode("person", [get_a_func], [], "human", [])
+
+        node = ProgramNode([human, person], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ["The Person type doesn't have a definition for a"])
+
+    def test_type_inherits_from_protocol(self):
+        scope = Scope()
+
+        protocol = ProtocolNode("ProtocolA", [])
+        type_a = TypeNode("TypeA", [], [], "ProtocolA")
+
+        node = ProgramNode([protocol, type_a], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ["Type TypeA can't inherit from protocol ProtocolA"])
+
+    def test_protocol_inherits_from_type(self):
+        scope = Scope()
+
+        type_a = TypeNode("TypeA", [], [])
+        protocol = ProtocolNode("ProtocolA", [], "TypeA")
+
+        node = ProgramNode([type_a, protocol], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ["Protocol ProtocolA can't inherit from type TypeA"])
+
+    def test_protocol_inherits_from_protocol(self):
+        scope = Scope()
+
+        protocol_base = ProtocolNode("ProtocolBase", [])
+        protocol = ProtocolNode("ProtocolA", [], "ProtocolBase")
+
+        node = ProgramNode([protocol_base, protocol], NumberNode(2))
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [])
+
+    def test_protocol_polymorphism_correct(self):
+        scope = Scope()
+
+        protocol = ProtocolNode("Flyable", [ProtocolMethodNode("fly", [ParameterNode("distance", "Number")], "void")])
+
+        fly_type_func = FunctionNode("fly", [ParameterNode("distance", "Number")],
+                                     FunctionCallNode("print", [StringNode("temp")]), "void")
+        type_a = TypeNode("Bird", [fly_type_func], [])
+
+        let_ = LetNode([ParameterNode("rocio", "Flyable")], [NewNode("Bird", [])],
+                       TypeFunctionCallNode(VariableNode("rocio"), "fly", [NumberNode(2)]))
+
+        node = ProgramNode([protocol, type_a], let_)
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [])
+
+    def test_protocol_polymorphism_wrong_method(self):
+        scope = Scope()
+
+        protocol = ProtocolNode("Flyable", [ProtocolMethodNode("fly", [ParameterNode("distance", "Number")], "void")])
+
+        fly_type_func = FunctionNode("walk", [ParameterNode("distance", "Number")],
+                                     FunctionCallNode("print", [StringNode("temp")]), "void")
+        type_a = TypeNode("Bird", [fly_type_func], [])
+
+        let_ = LetNode([ParameterNode("rocio", "Flyable")], [NewNode("Bird", [])],
+                       TypeFunctionCallNode(VariableNode("rocio"), "fly", [NumberNode(2)]))
+
+        node = ProgramNode([protocol, type_a], let_)
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, ['Flyable expression was expected instead of Bird'])
+
+    def test_is_expression(self):
+        scope = Scope()
+
+        is_expression = IsExpression(VariableNode("instance"), "NonExistentType")
+        node = ProgramNode([], is_expression)
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [
+            "The instance variable doesn't exist in the current context",
+            "The NonExistentType type doesn't exist in the current context"
+        ])
+
+    def test_ultimate(self):
+        scope = Scope()
+
+        """
+        protocol BaseProtocol{
+            method(param: Number): void
+        }
+        
+        protocol ChildProtocol extends BaseProtocol{
+        }
+        
+        type TypeA {
+            method(param: Number){
+                print("temp")
+            }
+            
+            another(){
+                5;
+            }
+        }
+        
+        for i in range(0, 10){
+            let instance: BaseProtocol = new TypeA() in 
+                if i < 2 instance.method(2) 
+                elif i >= 4 (instance as TypeA).another() 
+                else "Temp";
+        }
+        """
+
+        base_protocol = ProtocolNode("BaseProtocol",
+                                     [ProtocolMethodNode("method", [ParameterNode("param", "Number")], "void")])
+
+        child_protocol = ProtocolNode("ChildProtocol", [], "BaseProtocol")
+
+        method_func = FunctionNode("method", [ParameterNode("param", "Number")],
+                                   FunctionCallNode("print", [StringNode("temp")]), "void")
+
+        type_a = TypeNode("TypeA", [method_func, FunctionNode("another", [], NumberNode(5))], [])
+
+        conditions = [ComparationExpression("<", VariableNode("i"), NumberNode(2)),
+                      ComparationExpression(">=", VariableNode("i"), NumberNode(4))]
+
+        first_branch = TypeFunctionCallNode(VariableNode("instance"), "method", [NumberNode(2)])
+        second_branch = TypeFunctionCallNode(AsNode(VariableNode("instance"), "TypeA"), "another", [])
+        third_branch = StringNode("Temp")
+
+        expressions = [first_branch, second_branch, third_branch]
+
+        if_ = IfElseExpression(conditions, expressions)
+
+        let_params = [ParameterNode("instance", "BaseProtocol")]
+        value_params = [NewNode("TypeA", [])]
+        let = LetNode(let_params, value_params, if_)
+
+        for_node = ForNode("i", FunctionCallNode("range", [NumberNode(0), NumberNode(10)]), let)
+
+        node = ProgramNode([base_protocol, child_protocol, type_a], for_node)
+        result = self.visitor.visit(node, scope)
+
+        self.assertEqual(result, [])
 
 
 if __name__ == '__main__':
